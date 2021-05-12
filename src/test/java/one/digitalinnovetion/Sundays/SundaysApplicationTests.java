e192c41126f12c030120c75e7d642eb42887437c
Jackson <jacksontenorio8@gmail.com>
2021-05-12 15:35:44 -0300

    Sundays
    Projeto de gerenciamento de pessoas

---
+++ src/test/java/one/digitalinnovetion/Sundays/SundaysApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovetion.Sundays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
