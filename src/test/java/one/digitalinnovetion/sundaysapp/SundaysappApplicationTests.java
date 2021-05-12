bb08c7b8069dabcde2cbcf277c201766709811cf
Jackson <jacksontenorio8@gmail.com>
2021-05-12 20:05:20 -0300

    Update

--- src/test/java/one/digitalinnovetion/sundaysapp/SundaysappApplicationTests.java
+++ src/test/java/one/digitalinnovetion/sundaysapp/SundaysappApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SundaysApplicationTests {
+class SundaysappApplicationTests {
 
 	@Test
 	void contextLoads() {
