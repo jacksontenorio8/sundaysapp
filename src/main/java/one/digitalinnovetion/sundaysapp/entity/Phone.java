d05d4995301bab2fe7c2fccd6586f1eef6539b4b
Jackson <jacksontenorio8@gmail.com>
2021-05-12 19:12:45 -0300

    Projeto

---
+++ src/main/java/one/digitalinnovetion/sundaysapp/entity/Phone.java
@@ -0,0 +1,36 @@
+package one.digitalinnovetion.sundaysapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovetion.sundaysapp.enums.PhoneType;
+
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor;
+
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType Type;
+
+    @Column(nullable = false)
+    private String number;
+}
